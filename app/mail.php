b1cbccd7545f2ef9a364a88f7fcb9e912e79766b
den <trinitiwowka@gmail.com>
2016-11-24 17:53:16 +0200

    update mail.php

--- app/mail.php
+++ app/mail.php
@@ -1,19 +1,49 @@
 <?php
+    $surname = $_POST['surname'];
+	$name = $_POST['name'];
+    $patronymic = $_POST['patronymic'];
+    $auto = $_POST['auto'];
+    $skill = $_POST['skill'];
+    $phone = $_POST['phone'];
 
-    $sendto   = "za-rabotoy@yandex.ru";
-    $userphone = $_POST['phone'];
-    $username = $_POST['name'];
+    $admin_email="trinitiwowka@gmail.com";
+    $message .= "<tr>" .
+        "<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Имя</b></td>" .
+        "<td style='padding: 10px; border: #e9e9e9 1px solid;'>$name</td>" .
+        "</tr>".
+		"<tr>" .
+        "<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Фамилия</b></td>" .
+        "<td style='padding: 10px; border: #e9e9e9 1px solid;'>$surname</td>" .
+        "</tr>".
+		"<tr>" .
+        "<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Отчество</b></td>" .
+        "<td style='padding: 10px; border: #e9e9e9 1px solid;'>$patronymic</td>" .
+        "</tr>".
+        "<tr>" .
+        "<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Авто</b></td>" .
+        "<td style='padding: 10px; border: #e9e9e9 1px solid;'>$auto</td>" .
+        "</tr>".
+        "<tr>" .
+        "<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Стаж вожденич</b></td>" .
+        "<td style='padding: 10px; border: #e9e9e9 1px solid;'>$skill</td>" .
+        "</tr>".
+        "<tr>" .
+        "<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Телефон</b></td>" .
+        "<td style='padding: 10px; border: #e9e9e9 1px solid;'>$phone</td>" .
+        "</tr>";
 
-    // Формирование заголовка письма
-    $subject  = "Новое сообщение";
-    $headers  = "From: crossi.ru\r\n";
+    $message = "<table style='width: 100%;'>$message</table>";
 
-    $headers .= "Content-Type: text/plain;charset=utf-8 \r\n";
+    function adopt($text) {
+        return '=?UTF-8?B?'.Base64_encode($text).'?=';
+    }
 
-    // Формирование тела письма
-    $msg="Заказчик: $username \nЕго номер телефона: $userphone\n\n";
+    $form_subject = "Заявка такси";
 
-    // отправка сообщения
-    @mail($sendto, $subject, $msg, $headers)
+    $headers = "MIME-Version: 1.0" . PHP_EOL .
+        "Content-Type: text/html; charset=utf-8" . PHP_EOL .
+        'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
+        'Reply-To: '.$admin_email.'' . PHP_EOL;
 
+    mail($admin_email, adopt($form_subject), $message, $headers );
 ?>
